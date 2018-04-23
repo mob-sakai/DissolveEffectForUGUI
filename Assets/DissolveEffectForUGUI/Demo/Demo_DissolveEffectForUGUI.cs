2fd32f1b58f4dd7a2decfd04cf6bf065c36c1778
Takashi Sakai <sakai@mobcast.jp>
2018-04-23 22:14:47 +0900

    Added demo

---
+++ Assets/DissolveEffectForUGUI/Demo/Demo_DissolveEffectForUGUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Coffee.UIExtensions
+{
+	public class Demo_DissolveEffectForUGUI : MonoBehaviour
+	{
+		public void SetTimeScale(float scale)
+		{
+			Time.timeScale = scale;
+		}
+	}
+}
